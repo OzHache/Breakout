584fba8259a35c35567b005b035f0c1774f29722
OzHache <OzHache@gmail.com>
2019-01-30 12:01:36 -0800

    Wednesday

---
+++ BreakOut Build/Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed = 8f;
+    public GameObject Paddle;
+    public bool inPlay = false;
+
+    Rigidbody2D rb;         //assign in start
+    private float velX = 0;
+    private float velY = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();   //assigned once
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Launch()
+    {
+        //Rotate to a random direction
+        transform.rotation = Quaternion.Euler(Vector3.forward * UnityEngine.Random.Range(-60f, 60f));
+        //Send the ball flying
+
+        //set inplay to true
+    }
+}
--- BreakOut Build/Assets/Scripts/GameManager.cs
+++ BreakOut Build/Assets/Scripts/GameManager.cs
@@ -11,6 +11,8 @@ public class GameManager : MonoBehaviour
     public Rigidbody2D Paddle;  //Add Ref to Paddle
     //public Ball ball;         //todo: Add Ball class
     public static GameManager GetGameManager;
+    [Range(1f, 10f)]
+    public float Speed = 8;
 
     private int score = 0;      //values to track ui Values 
     private int level = 0;
@@ -27,7 +29,25 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(this != GetGameManager)
+        {
+            Destroy(this);
+        }
+    }
+    private void FixedUpdate()
+    {
+        MovePaddle();
+    }
+    void MovePaddle()
+    {
+        if (Input.GetAxisRaw("Horizontal") != 0f)
+        {
+            Paddle.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * Speed, 0f);
+        }
+        else
+        {
+            Paddle.velocity = Vector2.zero;
+        }
     }
 
     void UpdateUI()
